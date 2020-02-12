0e9ae864f0c347f0f9f684ce031f610d7d564e18
Win Min Htut <duwon119address@gmail.com>
2020-02-12 19:06:47 +0630

    sd

--- app.js
+++ app.js
@@ -239,9 +239,7 @@ function handlePostback(sender_psid, received_postback) {
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
   } else if (payload === 'get_started') {
-    response = { "text": "Hi, You are warmly welcomed."  }
-  } else if (payload === 'get_started') {
-    response = { "buttons":[
+    response = { "text": "Hi, You are warmly welcomed." + "buttons":[
               {
                 "type":"postback",
                 "title":"Main Menu",
@@ -257,8 +255,8 @@ function handlePostback(sender_psid, received_postback) {
                 "title":"About us",
                 "payload":"three"
               }                            
-            ]        }
-  }
+            ] }
+  
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
 }
