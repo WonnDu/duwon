5be3bb17a2ab4beca670553955947aff8ed8192f
Win Min Htut <duwon119address@gmail.com>
2020-02-14 16:05:32 +0630

    jehe

--- app.js
+++ app.js
@@ -287,8 +287,8 @@ function handlePostback(sender_psid, received_postback) {
         }
    }
   }
-callSend(sender_psid, response1).then(()=>{
-  return callSend(sender_psid, response2);
+callSendAPINew(sender_psid, response1).then(()=>{
+  return callSendAPINew(sender_psid, response2);
 });
   } else if (payload === 'onee') {
      response = { "attachment": {
