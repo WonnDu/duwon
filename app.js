891cab4af4623401a207e61a822c4ecd43d77419
Win Min Htut <duwon119address@gmail.com>
2020-02-20 21:20:06 +0630

    ejbjbe

--- app.js
+++ app.js
@@ -271,7 +271,7 @@ function handlePostback(sender_psid, received_postback) {
       "type":"template",
       "payload":{
         "template_type":"button",
-        "text":"",
+        "text":".",
          "buttons":[
                     {
                     "type":"postback",
