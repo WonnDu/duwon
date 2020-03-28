ba24da88a8187bb0cfb72758a9d99ebe5a224410
WonnDu <60248436+WonnDu@users.noreply.github.com>
2020-03-28 15:38:15 +0630

    Update app.js
    test

--- app.js
+++ app.js
@@ -2385,6 +2385,28 @@ else if (payload === 'innnter') {
                       ]
       }
   }
+    else if (payload === 'tselpob') {
+        response = {
+                  "text": "Please tell the area of yard:",
+                    "quick_replies": [
+                        {
+                          "content_type": "text",
+                          "title": "150-150 ft",
+                          "payload": "150ottp",
+                        },
+                         {
+                          "content_type": "text",
+                          "title": "100-100 ft",
+                          "payload": "100ottp",
+                        },
+                        {
+                          "content_type": "text",
+                          "title": "80-80 ft",
+                          "payload": "80ottp",
+                        },
+                      ]
+      }
+  }
    else if (payload === 'laan2') {
     response = { "attachment": {
                   "type": "template",
