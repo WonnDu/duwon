12f4ca88c719afc7ef91afe8660b98c73be18a5c
WonnDu <60248436+WonnDu@users.noreply.github.com>
2020-03-29 17:34:36 +0630

    Update app.js

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ let toselhou_byuser = {
   photos_ott:false,
   attach_Hou:false,
   ph_num:false,
+  for_both_room:false,
 }
 let userEntered_Hou_tosel = {};
 
@@ -1698,25 +1699,38 @@ else if (received_message.payload === "ottwp") {
 
 
 // for master bed room
- else if (received_message.payload === "tosel_hou_tell_mb" || received_message.payload === "tosel_hou_tell_both") {    
+ else if (received_message.payload === "tosel_hou_tell_mb") {    
     response = {
       "text": "How many master bed rooms in your house?"
     }
     received_message.payload = false;
     toselhou_byuser.area_hou_inOtt = true;
   }
+
   // for bed room
-   else if (received_message.payload === "tosel_hou_bedRoom" || received_message.payload === "tosel_hou_tell_both") {    
+   else if (received_message.payload === "tosel_hou_bedRoom") {    
     response = {
       "text": "How many bed rooms in your house?"
     }
     received_message.payload = false;
     toselhou_byuser.area_hou_inOtt = true;
   }
+
   // for both master bed room and bed room
-/* else if (payload === "tosel_hou_tell_both") {
-   response  = { "text": "Please chose one of the service charges that you want to know." } 
-  } */
+ else if (received_message.payload === "tosel_hou_tell_both") {
+   response  = { "text": "How many master bed rooms in your house?" 
+  }
+  received_message.payload = false;
+  toselhou_byuser.for_both_room = true;
+}
+ else if (received_message.text && toselhou_byuser.for_both_room === true) {  
+  userEntered_Hou_tosel.for_both_room = received_message.text; 
+    response = {
+      "text": "How many bed rooms in your house?"
+    }
+    received_message.payload = false;
+    toselhou_byuser.area_hou_inOtt = true;
+  } 
 
 
  else if (received_message.text && toselhou_byuser.area_hou_inOtt === true) { 
