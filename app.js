87d3d37bd77b101d2f867d67b5d3bdf7c38a7fe2
WonnDu <60248436+WonnDu@users.noreply.github.com>
2020-04-11 16:34:07 +0630

    Update app.js

--- app.js
+++ app.js
@@ -11822,7 +11822,7 @@ async function greetUser(sender_psid){
       "type":"template",
       "payload":{
         "template_type":"button",
-        "text":"Hi, "+user.first_name+" "+user.last_name+" are warmly welcomed. Thank you for contacting us. Have a nice day!",
+        "text":"Hi "+user.first_name+" "+user.last_name+", you are warmly welcomed. Thank you for contacting us. Have a nice day!",
          "buttons":[
                     {
                     "type":"postback",
@@ -11848,7 +11848,6 @@ async function greetUser(sender_psid){
       "type":"template",
       "payload":{
         "template_type":"button",
-        "text":".",
          "buttons":[
                     {
                     "type":"postback",
