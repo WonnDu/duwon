503ec1f8c1b2fccab3fa7d55f1ee43520259ca15
WonnDu <60248436+WonnDu@users.noreply.github.com>
2020-04-11 16:39:37 +0630

    Update app.js

--- app.js
+++ app.js
@@ -11848,7 +11848,7 @@ async function greetUser(sender_psid){
       "type":"template",
       "payload":{
         "template_type":"button",
-        "text":" ",
+        "text":". ",
          "buttons":[
                     {
                     "type":"postback",
